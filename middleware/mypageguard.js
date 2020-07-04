310bcfe2d49abe04895ab25577a60eb58bb12b1f
masaki-michida <stupid763@gmail.com>
2020-07-04 17:18:26 +0900

    add middleware permit user loged in to access post todo page

---
+++ middleware/mypageguard.js
@@ -0,0 +1,23 @@
+export default function ({
+  store,
+  redirect,
+  route
+
+}) {
+  store.watch(
+    state => state.currentUser,
+    (newUser, oldUser) => {
+      if (!newUser) {
+        switch (route.name) {
+          case 'index':
+            redirect('/login')
+            break
+          case 'mypage':
+            redirect('/login')
+            break
+          default:
+        }
+      }
+    }
+  )
+}
